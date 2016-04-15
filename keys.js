e65c37ed6aee395d89ed8bf1aba5921a81f36548
Bilal Mian <bilalj.mian@gmail.com>
2016-04-15 15:31:49 -0400

    initial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'PG8cye33Mse3h0DjKft8Pw9Xd',
+  consumer_secret: 'kDETmPEIy4GW4qu7jMSsXVTNM0LeUAtFhm2fOvquhbDxXwfoAd',
+  access_token_key: '19668578-TDfwAeu4b25z5gh8plo7rOSyott6uJdu2Jc1NOYii',
+  access_token_secret: '1Qc6bMJwL5NOaXkmRTRa9oxH2RJKDof5A2Jnxi4c3JSBZ',
+}
---
+++ liri.js
@@ -0,0 +1,24 @@
+var keys = require('./keys.js');
+var fs = require('fs');
+
+//Twitter API Function
+function twitter(){
+
+	var
+
+}
+
+//Spotify API Function
+function spotify(){
+
+}
+
+//OMDB API movie function
+function omdB(){
+
+}
+
+//Google API Function
+function google() {
+
+}
